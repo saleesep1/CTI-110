88256f15985e38af65d346ceeaa0dfaf9d20fedf
saleesep1 <saleesep1994@gmail.com>
2025-02-05 10:36:01 -0500

    Add files via upload

---
+++ P1LAB1_PhillipsSaleese.py
@@ -0,0 +1,9 @@
+# Saleese Phillips
+# February 4, 2025
+# P1LAB1
+# First & Last name input and welcome message
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome to this course, " + first_name + " " + last_name + "!")
