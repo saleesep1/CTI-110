72a178330d6835bc181676bd14ffde95bb055aee
saleesep1 <saleesep1994@gmail.com>
2025-02-16 10:11:57 -0500

    Add files via upload

---
+++ P1HW1_PhillipsSaleese.py
@@ -0,0 +1,22 @@
+# Saleese Phillips
+# 02/16/2025
+# P1HW1
+
+# This prgram does some math stuff
+
+# Exponentiation Section
+print("----Calculating Exponents----")
+
+base = int(input("Enter base number: "))
+exp = int(input("Enter exponent number: "))
+power = base ** exp
+print(base, "raised to the power of", exp, "is", power, "!!")
+
+# Addition and Subtraction Section
+print("----Addition and Subtraction----")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter number to add: "))
+num3 = int(input("Enter number to subtract: "))
+sum = num1 + num2 - num3
+print(num1, "+", num2, "-", num3, "equals", sum)
